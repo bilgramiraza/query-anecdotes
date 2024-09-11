b6e64ca91cd78c5ef4dd010b40262347839395be
Raza <bilgramiraza@yahoo.com>
2024-09-11 17:14:36 +0530

    Setup Basic RQ systems for Fetching Anecdotes from the DB

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,46 @@
+import { useQuery } from '@tanstack/react-query'
 import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
+import axios from 'axios';
 
 const App = () => {
+
+  const result = useQuery({
+    queryKey: ['anecdotes'],
+    queryFn: () => axios.get('http://localhost:3001/anecdotes').then(res => res.data),
+    retry: false,
+  });
+  console.log(JSON.parse(JSON.stringify(result)));
 
   const handleVote = (anecdote) => {
     console.log('vote')
   }
 
-  const anecdotes = [
-    {
-      "content": "If it hurts, do it more often",
-      "id": "47145",
-      "votes": 0
-    },
-  ]
+  const anecdotes = result.data;
+
+  if (result.isError) return <div>Anecdotes not available due to Server Issues</div>;
 
   return (
     <div>
       <h3>Anecdote app</h3>
-    
+
       <Notification />
       <AnecdoteForm />
-    
-      {anecdotes.map(anecdote =>
-        <div key={anecdote.id}>
-          <div>
-            {anecdote.content}
-          </div>
-          <div>
-            has {anecdote.votes}
-            <button onClick={() => handleVote(anecdote)}>vote</button>
-          </div>
-        </div>
-      )}
+
+      {
+        result.isLoading ?
+          <div>Loading Data...</div> :
+          anecdotes.map(anecdote =>
+            <div key={anecdote.id}>
+              <div>
+                {anecdote.content}
+              </div>
+              <div>
+                has {anecdote.votes}
+                <button onClick={() => handleVote(anecdote)}>vote</button>
+              </div>
+            </div>)
+      }
     </div>
   )
 }
